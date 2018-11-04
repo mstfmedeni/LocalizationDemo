14c25c51e1ba97b3af3aa35e6f66c51f514e09f6
mustafa MEDENi <mustafa.medeni@hotmail.com>
2018-11-05 02:08:53 +0300

    Initial Commit

---
+++ LocalizationDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LocalizationDemo
+//
+//  Created by Mustafa Medeni on 5.11.2018.
+//  Copyright © 2018 Mustafa Medeni. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
